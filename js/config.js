61cdf6d5610c433900c8fe691d83868327fe3418
HVZE <xstyleshvze@gmail.com>
2026-06-23 17:05:22 +0300

    Enable Yandex Metrika

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@ window.SITE_CONFIG = {
     telegram: '@HVZEweb',
     telegramUrl: 'https://t.me/HVZEweb',
     email: 'hvzeweb@mail.ru',
-    yandexMetrikaId: '', // например: '12345678' — оставьте пустым, пока не создадите счётчик
+    yandexMetrikaId: '110092464', // например: '12345678' — оставьте пустым, пока не создадите счётчик
 };
